0e503b2406cea2ac441c1c6bbbb4eaa02dc4f9e5
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2018-07-05 18:22:32 +0200

    Version 1.3.4
    Changed so that the TextField's for the Settings aren't created dynamically anymore

--- JungscharProtokoll/src/jungscharprotokoll/java/controller/FXMLSettingsController.java
+++ JungscharProtokoll/src/jungscharprotokoll/java/controller/FXMLSettingsController.java
@@ -5,6 +5,7 @@
  */
 package jungscharprotokoll.java.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -16,10 +17,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import jungscharprotokoll.java.dbConnection.DatabaseConnection;
+import jungscharprotokoll.java.model.Model;
 
 /**
  * FXML Controller class
@@ -28,10 +30,21 @@ import jungscharprotokoll.java.dbConnection.DatabaseConnection;
  */
 public class FXMLSettingsController implements Initializable {
 
-    @FXML
-    private TitledPane addLeiter;
+    private AnchorPane addLeiter;
     
     private final DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+    @FXML
+    private AnchorPane leiterView;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtPosition;
+    @FXML
+    private TextField txtGruppe;
     
 
     /**
@@ -39,64 +52,31 @@ public class FXMLSettingsController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        fillPane();
-    }    
-    
-    private void fillPane(){
-        HBox box = new HBox();
-        addLeiter.setContent(box);
-        VBox vBox = new VBox();
-        box.getChildren().add(vBox);
-        HBox hBoxName = new HBox();
-        HBox hBoxLastName = new HBox();
-        HBox hBoxEmail = new HBox();
-        HBox hBoxPosition = new HBox();
-        HBox hBoxGruppe = new HBox();
-        
-        vBox.getChildren().addAll(hBoxName,hBoxLastName, hBoxEmail, hBoxPosition, hBoxGruppe);
-        
-        Label name = new Label("Name: ");
-        TextField txtName = new TextField();
-        hBoxName.getChildren().addAll(name, txtName);
-        
-        Label lastName = new Label("Nachname: ");
-        TextField txtLastName = new TextField();
-        hBoxLastName.getChildren().addAll(lastName, txtLastName);
-        
-        Label email = new Label("EMail: ");
-        TextField txtEmail = new TextField();
-        hBoxEmail.getChildren().addAll(email, txtEmail);
-        
-        Label position = new Label("Position: ");
-        TextField txtPosition = new TextField();
-        hBoxPosition.getChildren().addAll(position, txtPosition);
-        
-        Label gruppe = new Label("Gruppe: ");
-        TextField txtGruppe = new TextField();
-        hBoxGruppe.getChildren().addAll(gruppe, txtGruppe);
-        
-        Button save = new Button();
-        save.setText("Speichern");
-        box.getChildren().add(save);
-        save.setOnAction((ActionEvent e) -> {
-            try {
-                writeToDatabase(vBox);
-            } catch (SQLException ex) {
-                Logger.getLogger(FXMLSettingsController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-        
     }
     
-    private void writeToDatabase(VBox box) throws SQLException{
-        String name = ((TextField) ((HBox) box.getChildren().get(0)).getChildren().get(1)).getText();
-        String lastName = ((TextField) ((HBox) box.getChildren().get(1)).getChildren().get(1)).getText();
-        String email = ((TextField) ((HBox) box.getChildren().get(2)).getChildren().get(1)).getText();
-        String position = ((TextField) ((HBox) box.getChildren().get(3)).getChildren().get(1)).getText();
-        String gruppe = ((TextField) ((HBox) box.getChildren().get(4)).getChildren().get(1)).getText();
+    private void writeToDatabase() throws SQLException{
+        String name = txtName.getText();
+        String lastName = txtLastName.getText();
+        String email = txtEmail.getText();
+        String position = txtPosition.getText();
+        String gruppe = txtGruppe.getText();
         
         dbConnection.connectToMysql("localhost", "jungschar", "root", "");
         dbConnection.writeNewLeiter(name, lastName, email, gruppe, position);
+    }
+
+    @FXML
+    private void back(ActionEvent event) throws IOException {
+        new Model().openNewWindow("FXMLStart.fxml", "Start");
+    } 
+
+    @FXML
+    private void newLeiter(ActionEvent event) {
+    }
+
+    @FXML
+    private void save(ActionEvent event) throws SQLException {
+        writeToDatabase();
     }
     
 }
